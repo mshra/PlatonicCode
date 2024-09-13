d3b7cb78710ddf1a15db1b55c7d5f2ff4e2fc742
simarjeet <simarjeet.sm7@gmail.com>
2024-09-13 21:15:40 +0530

    Added Nextauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+
+
 export default function Home() {
+
   return (
     <>
       <div className="flex justify-center items-center h-full w-full">
         hello, world
+      
       </div>
     </>
   );
