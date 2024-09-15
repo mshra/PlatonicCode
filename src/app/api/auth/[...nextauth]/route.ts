33b0ffbf952e097fc9455c9761136464e41efcec
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-09-15 18:55:59 +0530

    edits

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,23 @@
-// import NextAuth from "next-auth";
+import NextAuth from "next-auth";
 
-// import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
-// const handler = NextAuth({
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-//     }),
-//   ],
-//   callbacks: {
-//     async redirect({ url, baseUrl }) {
-//       return "/";
-//     },
-//   },
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  // callbacks: {
+  //   async redirect({ url, baseUrl }) {
+  //     return "/";
+  //   },
+  // },
 
-//   pages: {
-//     signIn: "/signin",
-//   },
-// });
+  pages: {
+    signIn: "/signin",
+  },
+});
 
-// export { handler as GET, handler as POST };
+export { handler as GET, handler as POST };
