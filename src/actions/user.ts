88894b42eb2b70f10c2405b406157085bda94263
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-09-20 12:02:23 +0530

    edits

---
+++ src/actions/user.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { getUserByEmail } from "@/db/queries/select";
+
+type UserParam = {
+  id: number;
+  name: string;
+  email: string;
+  image: string;
+};
+
+export async function getUserProfilePicture(param: UserParam) {
+  const user = await getUserByEmail(param.email);
+}
