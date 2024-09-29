b0b8918230b8577a7dea3758a2d48cd9df4eaf5f
tarzi <aaryan.mshra@gmail.com>
2024-09-29 17:11:01 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className="overflow-x-hidden">
+      <body className="overflow-y-hidden">
         <Providers>{children}</Providers>
       </body>
     </html>
