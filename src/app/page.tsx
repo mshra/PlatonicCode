0b9fd7adc43eac4ab98bed6f3863f14b60eb1715
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-09-29 15:13:04 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
 "use client";
 import Navbar from "@/components/Navbar/Navbar";
-import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (session) {
-    redirect("/topics");
-  }
 
   return (
     <>
