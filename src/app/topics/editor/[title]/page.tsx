28194948260fded45d6620b8da08bc575afa38a1
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-10-01 19:08:41 +0530

    changes

--- src/app/topics/editor/[title]/page.tsx
+++ src/app/topics/editor/[title]/page.tsx
@@ -1,18 +1,18 @@
 "use client";
+import { runThisCode } from "@/actions/judge";
+import Chat from "@/components/Chat";
+import { EditorMenubar } from "@/components/Editor-Menubar";
 import Navbar from "@/components/Navbar/Navbar";
-import { Editor } from "@monaco-editor/react";
-import { editor } from "monaco-editor";
+import TestCase from "@/components/TestCase";
 import {
   ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
-import Chat from "@/components/Chat";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Editor } from "@monaco-editor/react";
+import { editor } from "monaco-editor";
 import { useRef } from "react";
-import { Button } from "@/components/ui/button";
-import { runThisCode } from "@/actions/judge";
-import TestCases from "@/components/test-cases";
-import { Play } from "lucide-react";
 
 export default function ProblemEditor({
   params,
@@ -33,8 +33,8 @@ export default function ProblemEditor({
 
   return (
     <div className="grid grid-rows-[auto_1fr] h-full text-xl">
-      <div className="mb-2">
-        <Navbar />
+      <div className="m-2">
+        <EditorMenubar />
       </div>
       <ResizablePanelGroup direction="horizontal">
         <ResizablePanel defaultSize={50}>
@@ -43,26 +43,31 @@ export default function ProblemEditor({
         <ResizableHandle />
         <ResizablePanel defaultSize={50}>
           <ResizablePanelGroup direction="vertical">
-            <ResizablePanel defaultSize={70}>
+            <ResizablePanel defaultSize={60}>
               <div className="flex h-full items-center justify-center p-2">
                 <Editor
                   theme="vs-dark"
-                  defaultLanguage="cpp"
+                  defaultLanguage="python"
                   defaultValue="print('hello, world')"
                   onMount={handleEditorMount}
                 />
               </div>
             </ResizablePanel>
             <ResizableHandle />
-            <ResizablePanel defaultSize={30}>
-              <div className="h-full  p-6">
-              <Button className="flex justify-center items-center gap-1 hover:scale-110" onClick={handleClick}>
-                Run <Play size={15}/>
-              </Button>
-                <div>
-                  <TestCases testcases={testcases}/>
-                </div>
-                
+            <ResizablePanel defaultSize={40}>
+              <div className="h-full p-4">
+                <Tabs defaultValue="testcase">
+                  <TabsList>
+                    <TabsTrigger value="testcase">Testcase</TabsTrigger>
+                    <TabsTrigger value="result">Result</TabsTrigger>
+                  </TabsList>
+                  <TabsContent value="testcase">
+                    <TestCase />
+                  </TabsContent>
+                  <TabsContent value="result">
+                    Run the code first you fucking idiot.
+                  </TabsContent>
+                </Tabs>
               </div>
             </ResizablePanel>
           </ResizablePanelGroup>
@@ -71,21 +76,3 @@ export default function ProblemEditor({
     </div>
   );
 }
-
-
-
-
-export const testcases = [
-  {
-    input:`[1,2,4,5]`
-  }, 
-  {
-    input:`[1,2]`
-  }, 
-  {
-    input:`[1,2,4,]`
-  }, 
-  {
-    input:`[1]`
-  }
-]
