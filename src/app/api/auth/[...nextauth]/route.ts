19b51fd98058bb67c8320b3f2e8e81003f9cc506
Aaryan Mishra <aaryan.mshra@gmail.com>
2024-10-03 12:28:50 +0530

    fixed redirecting

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -21,9 +21,10 @@ const handler = NextAuth({
               name: profile?.name as string,
               email: profile?.email as string,
             });
+            return true;
           }
+          return "/topics";
         }
-        return "/topics";
       } catch (error) {
         console.error("Error creating user:", error);
         return false;
